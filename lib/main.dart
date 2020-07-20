a02a26016f7783ee416b5169aa99df62e6c75ca6
manan-bedi-2908 <mananbedilps@gmail.com>
2020-07-20 23:55:16 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
   @override
   Widget build(BuildContext context) {
+  var questions = ['What\'s your favorite color?', 
+                   'What\'s your favorite animal?',
+                  ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default home page!'),
+        body: Column(children: [
+          Text(questions[questionIndex]),
+          RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion,),
+          RaisedButton(child: Text('Answer 2'), onPressed: () => print("Answer Chosen 2"),),
+          RaisedButton(child: Text('Answer 3'), onPressed: () {
+              print("Answer 3 chosen!");
+            }
+          ,),
+        ], ),
       ),
     );
   }
