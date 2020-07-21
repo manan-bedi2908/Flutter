2d7a02eb9c7244b010a9356076db946359a6748d
manan-bedi-2908 <mananbedilps@gmail.com>
2020-07-21 23:28:01 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 // void main() {
 //   runApp(MyApp());
@@ -38,21 +39,10 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: () => print("Answer Chosen 2"),
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: () {
-                print("Answer 3 chosen!");
-              },
-            ),
+            Question(questions[_questionIndex],),
+            Answer(),
+            Answer(),
+            Answer(),
           ],
         ),
       ),
