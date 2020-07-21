7e0f69d8b4b783ccff5a03480656d33cb62d8534
manan-bedi-2908 <mananbedilps@gmail.com>
2020-07-21 23:10:49 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import './question.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -17,35 +16,45 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
   var _questionIndex = 0;
 
   void _answerQuestion() {
     setState(() {
-        _questionIndex = _questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-  var questions = ['What\'s your favorite color?', 
-                   'What\'s your favorite animal?',
-                  ];
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(children: [
-          Question(questions[_questionIndex]),
-          RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion,),
-          RaisedButton(child: Text('Answer 2'), onPressed: () => print("Answer Chosen 2"),),
-          RaisedButton(child: Text('Answer 3'), onPressed: () {
-              print("Answer 3 chosen!");
-            }
-          ,),
-        ], ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print("Answer Chosen 2"),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print("Answer 3 chosen!");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
