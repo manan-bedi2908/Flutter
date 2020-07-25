b857276413dad223dbd050d98f1b8d28786d22ff
manan-bedi2908 <53931841+manan-bedi2908@users.noreply.github.com>
2020-07-26 01:14:24 +0530

    added

--- lib/main.dart
+++ lib/main.dart
@@ -20,21 +20,46 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Lion', 'Dog', 'Rabbit', 'Cat'],
+      'answers': [
+        {'text': 'Lion', 'score': 10},
+        {'text': 'Tiger', 'score': 5},
+        {'text': 'Dog', 'score': 3},
+        {'text': 'Cat', 'score': 1},
+      ],
     },
     {
       'questionText': 'What\'s your favorite car?',
-      'answers': ['Audi', 'Mercedes', 'Lamborghini', 'Nano'],
+      'answers': [
+        {'text': 'Audi', 'score': 10},
+        {'text': 'Mercedes', 'score': 5},
+        {'text': 'BMW', 'score': 3},
+        {'text': 'Nano', 'score': 1},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -60,7 +85,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
